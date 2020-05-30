3a02b6546bb6e4e95a7922d40222132fc4d25f08
Nguyen Phuoc Duc <phuocduc2012@gmail.com>
2020-05-30 15:41:30 +0700

    add static root

--- myproject/settings.py
+++ myproject/settings.py
@@ -142,10 +142,14 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
-
+STATIC_ROOT = [
+    os.path.join(BASE_DIR, 'static'),
+]
+ 
 
 LOGOUT_REDIRECT_URL = 'home'
 LOGIN_REDIRECT_URL = 'home'
